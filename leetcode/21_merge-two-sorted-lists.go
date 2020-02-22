ddd1856d8440d2415a227714e5cb675beaf7daaf
gcy <gcy@zhihu.com>
2020-02-22 20:50:32 +0800

    add 21

---
+++ leetcode/21_merge-two-sorted-lists.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	head := (*ListNode)(nil) //
+	tail := (*ListNode)(nil) // 结果链表的 尾结点
+	for l1 != nil && l2 != nil {
+		t := &ListNode{}
+		if l1.Val < l2.Val {
+			t.Val = l1.Val
+			l1 = l1.Next
+		} else if l1.Val == l2.Val {
+			t.Val = l1.Val
+			l1 = l1.Next
+		} else {
+			t.Val = l2.Val
+			l2 = l2.Next
+		}
+		if head == nil {
+			head = t
+			tail = t
+		} else {
+			tail.Next = t
+			tail = tail.Next
+		}
+	}
+
+	if l1 != nil {
+		tail.Next = l1
+	}
+	if l2 != nil {
+		tail.Next = l2
+	}
+	return head
+}
+
+func printList(l *ListNode) {
+	for l != nil {
+		fmt.Println(l.Val)
+	}
+}
+
+func main() {
+
+}
