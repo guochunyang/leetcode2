0c8cc3889607d520b8aeb0c34f8d87ca56f78ac2
gcy <gcy@zhihu.com>
2020-02-22 21:03:33 +0800

    add 21

--- leetcode/21_merge-two-sorted-lists.go
+++ leetcode/21_merge-two-sorted-lists.go
@@ -15,7 +15,7 @@ type ListNode struct {
  * }
  */
 
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+func _mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l1 == nil {
 		return l2
 	}
@@ -55,6 +55,48 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	return head
 }
 
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	head := (*ListNode)(nil)
+	tail := (*ListNode)(nil)
+
+	t := (*ListNode)(nil)
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			t = l1
+			l1 = l1.Next
+		} else if l1.Val == l2.Val {
+			t = l1
+			l1 = l1.Next
+		} else {
+			t = l2
+			l2 = l2.Next
+		}
+		if head == nil {
+			head = t
+			tail = t
+			head.Next = tail
+		} else {
+			tail.Next = t
+			tail = t
+		}
+	}
+
+	if l1 != nil {
+		tail.Next = l1
+	}
+	if l2 != nil {
+		tail.Next = l2
+	}
+	return head
+}
+
 func printList(l *ListNode) {
 	for l != nil {
 		fmt.Println(l.Val)
@@ -62,5 +104,4 @@ func printList(l *ListNode) {
 }
 
 func main() {
-
 }
